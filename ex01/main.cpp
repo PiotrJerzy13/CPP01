142f53596035f1297f75ea1b13c721c4c75f6e9a
PiotrJerzy13 <138906583+PiotrJerzy13@users.noreply.github.com>
2024-12-18 22:17:14 +0100

    adding proper Makefile

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -3,10 +3,14 @@
 int	main(void)
 {
 	Zombie	*zombie;
+	int		i =0;
 
 	zombie = zombieHorde(5, "Tom");
-	for (int i = 0; i < 5; i++)
+	while (i < 5)
+	{
 		zombie[i].announce();
+		i++;
+	}
 	delete[] zombie;
 	return (0);
 }
