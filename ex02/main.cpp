12cb294662866671c0dbd2da0a4e0b04ce376dcc
PiotrJerzy13 <138906583+PiotrJerzy13@users.noreply.github.com>
2024-12-18 13:42:02 +0100

    ex 02

---
+++ ex02/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+void printInfo(const std::string& value, const std::string* stringPTR, const std::string& stringREF) {
+    std::cout << "The memory address of the string variable = " << &value << '\n';
+    std::cout << "The memory address held by stringPTR = " << stringPTR << '\n';
+    std::cout << "The memory address held by stringREF = " << &stringREF << '\n';
+    std::cout << "The value of the string variable = " << value << '\n';
+    std::cout << "The value pointed to by stringPTR = " << *stringPTR << '\n';
+    std::cout << "The value pointed to by stringREF = " << stringREF << '\n';
+}
+
+int main() {
+    std::string value = "HI THIS IS BRAIN";
+    std::string* stringPTR = &value;
+    std::string& stringREF = value;
+
+    printInfo(value, stringPTR, stringREF);
+
+    return 0;
+}
